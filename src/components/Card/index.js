ce82a0c65a50698e9b4c1977725617ae811fb7d5
leomoraiscam <leozonnn@gmail.com>
2020-07-01 17:21:01 -0500

    Implementando a funcionalidade de drag on drop

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 import { Container, Label } from "./styled";
 
 export default function Card({ data }) {
+  const [{ isDragging }, dragRef] = useDrag({
+    item: { type: "CARD" },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
   return (
     <>
-      <Container>
+      <Container ref={dragRef}>
         <header>
           {data.labels.map((label) => (
             <Label key={data.id} color={label} />
