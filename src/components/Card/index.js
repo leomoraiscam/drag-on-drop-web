8aa8401479235b8684331170e7af3f35d730c17c
leomoraiscam <leozonnn@gmail.com>
2020-07-04 10:13:07 -0500

    implementadno a movimentação dos cards e implementação do context

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useContext } from "react";
 import { useDrag, useDrop } from "react-dnd";
-
+import BoardContext from "../Board/context";
 import { Container, Label } from "./styles";
 
 export default function Card({ data, index }) {
   const ref = useRef();
+  const { move } = useContext(BoardContext);
 
   const [{ isDragging }, dragRef] = useDrag({
     item: { type: "CARD", index },
@@ -29,6 +30,8 @@ export default function Card({ data, index }) {
 
       if (draggedIndex < targetIndex && draggedTop < targetCenter) return;
       if (draggedIndex > targetIndex && draggedTop > targetCenter) return;
+
+      move(draggedIndex, targetIndex);
     },
   });
 
